34243fe8fd6016a312eb40a820e30737b2ec30fa
keviinramiirez <kevin.ramirez3@upr.edu>
2021-04-10 20:59:57 -0400

    handles component to display by recognizing route changes

---
+++ ezrum-app/src/util/ContentViews.js
@@ -0,0 +1,4 @@
+export const tutorViews = {
+  subjects: 'subjects',
+  requests: 'requests'
+}
--- ezrum-app/src/util/Util.js
+++ ezrum-app/src/util/Util.js
@@ -1,1 +1,7 @@
 export const navbarHeight = 70
+
+/** returns the slug of the given url. */
+export const urlSlug = (url) => url.substring(url.lastIndexOf('/') + 1)
+
+/** returns the given url without the slug. */
+export const domainUrl = (url) => url.substring(0, url.lastIndexOf('/'))
