93f2421a333b2dc82d065dbc2fba97002a01a395
keviinramiirez <kevin.ramirez3@upr.edu>
2021-04-26 10:36:19 -0400

    team-ezrum database addon configuration

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,9 +1,19 @@
+# d-ezrum
+# pg_config = {
+#     'user': 'pqmmupcmrcxuds',
+#     'password': 'dc38ccb93ea67c14e6688d32d0854953fd83f842016321a1d7c88f05d7f230a0',
+#     'dbname': 'd2e35du826u9uj',
+#     'dbport': 5432,
+#     'dbhost': 'ec2-52-21-153-207.compute-1.amazonaws.com'
+# }
+
+# team-ezrum
 pg_config = {
-    'user': 'pqmmupcmrcxuds',
-    'password': 'dc38ccb93ea67c14e6688d32d0854953fd83f842016321a1d7c88f05d7f230a0',
-    'dbname': 'd2e35du826u9uj',
+    'user': 'dzqcntuafklpsu',
+    'password': 'c85dc62ca4a6f892cd7f484611ab8911baf901bf6c175e0e1e42b1c605d0e78b',
+    'dbname': 'd7t5iim7ojbhs0',
     'dbport': 5432,
-    'dbhost': 'ec2-52-21-153-207.compute-1.amazonaws.com'
+    'dbhost': 'ec2-107-20-153-39.compute-1.amazonaws.com'
 }
 
 
