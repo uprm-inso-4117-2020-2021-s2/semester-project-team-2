5cc3f82f376705605dd999b5f40d233464f34fa1
keviinramiirez <kevin.ramirez3@upr.edu>
2021-04-28 17:32:49 -0400

    removed unused commented configuration

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,11 +1,3 @@
-# d-ezrum
-# pg_config = {
-#     'user': 'pqmmupcmrcxuds',
-#     'password': 'dc38ccb93ea67c14e6688d32d0854953fd83f842016321a1d7c88f05d7f230a0',
-#     'dbname': 'd2e35du826u9uj',
-#     'dbport': 5432,
-#     'dbhost': 'ec2-52-21-153-207.compute-1.amazonaws.com'
-# }
 
 # team-ezrum
 pg_config = {
